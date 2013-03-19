bfab9e1c23dd54ca1ecf580382d1f1da07d4d3c9
Domino <buzu.buzu.buzu@gmail.com>
2013-03-19 09:24:41 +0100

    Obracanie Kamer�
    Dodałem teraz taki plane, żeby się było łatwiej znaleźć w przestrzeni.
    pozatym dodałem także obracanie kamerą na numpadzie 7 i 9. Trzeba
    ogólnie ulepszyć te rozglądanie się...

--- Rybobranie/Rybobranie/Game1.cs
+++ Rybobranie/Rybobranie/Game1.cs
@@ -17,11 +17,12 @@ namespace Rybobranie
     public class Game1 : Microsoft.Xna.Framework.Game
     {
         private Model Rybcia;
+        private Model Plane;
         private Model EnvSph;
         private KeyboardState StanKlawiatury;
 
         private Matrix world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
-        private Matrix view = Matrix.CreateLookAt(new Vector3(10, 10, 10), new Vector3(0, 0, 0), Vector3.UnitY);
+        private Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);
         private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 2000f);
 
         private Vector3 translacja;
@@ -32,6 +33,7 @@ namespace Rybobranie
         GraphicsDeviceManager graphics;
         GraphicsDevice device;
         SpriteBatch spriteBatch;
+        private float myszkaRoll;
 
         public Game1()
         {
@@ -61,6 +63,7 @@ namespace Rybobranie
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             Rybcia = Content.Load<Model>("rybka");
+            Plane = Content.Load<Model>("plane");
             // TODO: use this.Content to load your game content here
         }
 
@@ -85,27 +88,28 @@ namespace Rybobranie
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
             KeyboardState stanKlawiatury = Keyboard.GetState();
-            if (stanKlawiatury.IsKeyDown(Keys.W)) translacja.Z = 0.5f;
-            if (stanKlawiatury.IsKeyDown(Keys.S)) translacja.Z = -0.5f;
-            if (stanKlawiatury.IsKeyDown(Keys.A)) translacja.X = 0.5f;
-            if (stanKlawiatury.IsKeyDown(Keys.D)) translacja.X = -0.5f;
-            if (stanKlawiatury.IsKeyDown(Keys.NumPad8)) myszkaY = 0.03f;
-            if (stanKlawiatury.IsKeyDown(Keys.NumPad2)) myszkaY = -0.03f;
-            if (stanKlawiatury.IsKeyDown(Keys.NumPad4)) myszkaX = 0.03f;
-            if (stanKlawiatury.IsKeyDown(Keys.NumPad6)) myszkaX = -0.03f;
+            if (stanKlawiatury.IsKeyDown(Keys.W)) translacja.Z = 0.3f;
+            if (stanKlawiatury.IsKeyDown(Keys.S)) translacja.Z = -0.3f;
+            if (stanKlawiatury.IsKeyDown(Keys.A)) translacja.X = 0.3f;
+            if (stanKlawiatury.IsKeyDown(Keys.D)) translacja.X = -0.3f;
+            if (stanKlawiatury.IsKeyDown(Keys.NumPad2)) myszkaY = 0.03f;
+            if (stanKlawiatury.IsKeyDown(Keys.NumPad8)) myszkaY = -0.03f;
+            if (stanKlawiatury.IsKeyDown(Keys.NumPad6)) myszkaX = 0.03f;
+            if (stanKlawiatury.IsKeyDown(Keys.NumPad4)) myszkaX = -0.03f;
+            if (stanKlawiatury.IsKeyDown(Keys.NumPad7)) myszkaRoll = -0.01f;
+            if (stanKlawiatury.IsKeyDown(Keys.NumPad9)) myszkaRoll = 0.01f;
             /*MouseState nowyStanMyszy = Mouse.GetState();
             if(nowyStanMyszy.LeftButton == ButtonState.Pressed)
             {
                 myszkaX = (staryStanMyszy.X - nowyStanMyszy.X) / 500.0f;
                 myszkaY = (staryStanMyszy.Y - nowyStanMyszy.Y) / 500.0f;
             }
-            */    
-
-            view *= Matrix.CreateFromYawPitchRoll(myszkaX, myszkaY, 0);
+            */
+            view *= Matrix.CreateFromYawPitchRoll(myszkaX, myszkaY, myszkaRoll);
             view *= Matrix.CreateTranslation(translacja);
 
             translacja.X = 0; translacja.Z = 0;
-            myszkaX = 0; myszkaY = 0;
+            myszkaX = 0; myszkaY = 0; myszkaRoll = 0;
 
             base.Update(gameTime);
         }
@@ -117,6 +121,7 @@ namespace Rybobranie
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             DrawModel(Rybcia, world, view, projection);
+            DrawModel(Plane, world, view, projection);
 
             // TODO: Add your drawing code here
 
